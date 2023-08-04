496fe7dadb8368144a2c139d7de59ad441a1d791
shindori fumiya <shindori238@gmail.com>
2023-08-04 20:04:52 +0900

    add variable

---
+++ Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string charaterName = "John";
+    int charaterAge;
+    charaterAge = 35;
+    cout << charaterName << charaterAge << endl;
+
+    return 1;
+}
